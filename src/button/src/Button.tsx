4c2115d4ba72817c1571e06dd22fb69bac436082
goliafrs <goliaf.rs@gmail.com>
2021-10-07 12:03:58 +0300

    Прогресс

--- src/button/src/Button.tsx
+++ src/button/src/Button.tsx
@@ -101,7 +101,7 @@ export default defineComponent({
       [`${name}--${props.color}`]: !!props.color
     }
 
-    const size = props.small ? 18 : props.large ? 26 : props.fab ? 30 : 22
+    const size: number = props.small ? 18 : props.large ? 26 : props.fab ? 30 : 22
 
     return () => <button
       tabindex={props.tabindex}
